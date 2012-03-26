e75114b3e24de0ab25fd9daeb3e98820c83a2a1c
C. Scott Ananian <cscott@cscott.net>
2012-03-26 16:06:14 -0400

    Refactor read.js.

--- read.js
+++ read.js
@@ -15,9 +15,9 @@ program
 var input_file = program.args[0];
 var data = JSON.parse(fs.readFileSync(input_file, 'utf-8'));
 
-function p(s) {
+var p = function(s) {
     console.log(s);
-}
+};
 
 p("<!DOCTYPE HTML>");
 p("<html><head><title>Character Visualization</title>");
@@ -28,16 +28,50 @@ p("<body><h1>Character Visualization</h1>");
 //p('Version', data.version);
 p("<p>" + data.set.length + "samples</p>");
 
-function normalize(data_set) {
-    function mkpt(p) { return { x:p[0], y:p[1] }; }
+var Point = function(x, y) {
+    this.x = x; this.y = y;
+};
+Point.prototype = {
+    clone: function() { return new Point(this.x, this.y); },
+    equals: function(p) { return Point.equals(this, p); }
+};
+Point.equals = function(a, b) { return (a.x ===b.x) && (a.y === b.y); };
+var Box = function(tl, br) {
+    this.tl = tl;
+    this.br = br;
+};
+Box.prototype = {
+    unionPt: function(pt) {
+        if (pt.x < this.tl.x) { this.tl.x = pt.x; }
+        if (pt.y < this.tl.y) { this.tl.y = pt.y; }
+        if (pt.x > this.br.x) { this.br.x = pt.x; }
+        if (pt.y > this.br.y) { this.br.y = pt.y; }
+    },
+    union: function(box) {
+        this.unionPt(box.tl);
+        this.unionPt(box.br);
+    },
+    size: function() {
+        return { width: this.br.x - this.tl.x,
+                 height: this.br.y - this.tl.y };
+    }
+};
+Box.fromPts = function(pts) {
+    // pts must have at least one element
+    var b = new Box(pts[0].clone(), pts[0].clone());
+    pts.forEach(function(p) { b.unionPt(p); });
+    return b;
+};
+
+var normalize = function(data_set) {
+    var mkpt = function(p) { return new Point(p[0], p[1]); };
 
     // remove dups
     data_set.strokes = data_set.strokes.map(function(stroke) {
         stroke = stroke.map(mkpt);
         var nstrokes = [stroke[0]];
         for (var i=1; i<stroke.length; i++) {
-            if (stroke[i].x == stroke[i-1].x &&
-                stroke[i].y == stroke[i-1].y)
+            if (stroke[i].equals(stroke[i-1]))
                 continue;
             nstrokes.push(stroke[i]);
         }
@@ -45,41 +79,33 @@ function normalize(data_set) {
     });
 
     // find bounding box
-    // Ugh: this should be a one-liner...
-    function minpt(pta, ptb) {
-        return {x:Math.min(pta.x, ptb.x), y:Math.min(pta.y, ptb.y)};
-    }
-    function maxpt(pta, ptb) {
-        return {x:Math.max(pta.x, ptb.x), y:Math.max(pta.y, ptb.y)};
-    }
-    var max = data_set.strokes.map(function(stroke) {
-        return stroke.reduce(maxpt);
-    }).reduce(maxpt);
-    var min = data_set.strokes.map(function(stroke) {
-        return stroke.reduce(minpt);
-    }).reduce(minpt);
+    var strokeBBs = data_set.strokes.map(function(stroke) {
+        return Box.fromPts(stroke);
+    });
+    var bbox = strokeBBs[0];
+    strokeBBs.forEach(function(bb) { bbox.union(bb); });
 
     // use correct aspect ratio
-    var x_size = (max.x - min.x), y_size = (max.y - min.y);
-    var size = Math.max(x_size, y_size);
-    function norm(pt) {
+    var size = bbox.size();
+    size = Math.max(size.width, size.height);
+    var norm = function(pt) {
         // map to [0-1], y=0 at bottom (math style)
-        var x = (pt.x - min.x) / size;
-        var y = (pt.y - min.y) / size;
-        return {x:x, y:y};
-    }
+        var x = (pt.x - bbox.tl.x) / size;
+        var y = (pt.y - bbox.tl.y) / size;
+        return new Point(x, y);
+    };
     // remove dups
     data_set.strokes = data_set.strokes.map(function(stroke) {
         return stroke.map(norm);
     });
-}
+};
 
-function smooth(data_set) {
+var smooth = function(data_set) {
     data_set.strokes = data_set.strokes.map(function(stroke) {
         var nstroke = [];
         for (var i=0; i<stroke.length; i++) {
-            var acc = { x:stroke[i].x * SMOOTH_ALPHA,
-                        y:stroke[i].y * SMOOTH_ALPHA };
+            var acc = new Point(stroke[i].x * SMOOTH_ALPHA,
+                                stroke[i].y * SMOOTH_ALPHA );
             var n = SMOOTH_N;
             // [0, 1, 2, 3, 4 ] .. N = 2, length=5
             while (n>0 && (i<n || i>=(stroke.length-n)))
@@ -94,10 +120,10 @@ function smooth(data_set) {
         }
         return nstroke;
     });
-}
+};
 
 var canvas_id = 0;
-function draw_letter(data_set, caption) {
+var draw_letter = function(data_set, caption) {
     // data_set should be normalized (range [0,1], dups removed)
     var id = "c" + (canvas_id++);
     p("<canvas id="+JSON.stringify(id)+" title="+JSON.stringify(caption)+"></canvas>");
@@ -107,8 +133,8 @@ function draw_letter(data_set, caption) {
     p("  canvas.width = canvas.height = "+CANVAS_RESOLUTION*RETINA_FACTOR+";");
     p("  var ctx = canvas.getContext(\"2d\");");
     var norm = function(pt) {
-        return { x: pt.x*CANVAS_RESOLUTION*RETINA_FACTOR,
-                 y: (1-pt.y)*CANVAS_RESOLUTION*RETINA_FACTOR };
+        return new Point(   pt.x *CANVAS_RESOLUTION*RETINA_FACTOR,
+                         (1-pt.y)*CANVAS_RESOLUTION*RETINA_FACTOR );
     };
     // set ctx.stroke style, whatever that property is.
     data_set.strokes.forEach(function(stroke) {
@@ -127,7 +153,7 @@ function draw_letter(data_set, caption) {
     p("  ctx.fillText("+JSON.stringify(data_set.source+" "+data_set.start)+", 0, 10, "+CANVAS_RESOLUTION*RETINA_FACTOR+");");
     p("})();");
     p("</script>");
-}
+};
 
 // okay, draw the letters!
 for (var i=0; i<data.set.length; i++) {
